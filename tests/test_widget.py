7130bbcfd32790a188ca2b3075ec826aa4232099
ALEXANDR_SH <sandro.86@bk.ru>
2025-12-27 13:57:03 +0300

    widget.py test

---
+++ tests/test_widget.py
@@ -0,0 +1,7 @@
+# from __future__ import annotations
+#
+# from src.widget import mask_account_card
+#
+#
+# # assert mask_account_card()
+# from __future__ import annotations
---
+++ tests/widget_test.py
@@ -0,0 +1,6 @@
+# from __future__ import annotations
+#
+# from src.widget import mask_account_card
+#
+#
+# assert mask_account_card()
