c85e800e703207651045af2f6a8bf46518ed47d8
ALEXANDR_SH <sandro.86@bk.ru>
2026-01-15 00:31:45 +0300

    Фикстура с тестовыми транзакциями.

--- tests/processing_test.py
+++ tests/processing_test.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from src.processing import filter_by_state
 
 
-def test_filter_by_state():
-    transactions = [
-        {"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
+def test_filter_by_state(transactions):
+    #     transactions = [
+    #         {"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    #         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    #         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    #         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    #     ]
 
     # Тест 1: фильтруем по 'EXECUTED'
     assert filter_by_state(transactions, "EXECUTED") == [
@@ -24,4 +24,4 @@ def test_filter_by_state():
     ]
 
     # Тест 3: фильтруем по несуществующему состоянию (должен вернуть пустой список)
-    assert filter_by_state(transactions, "PENDING") == []
+    assert [] == filter_by_state(transactions, "PENDING")
